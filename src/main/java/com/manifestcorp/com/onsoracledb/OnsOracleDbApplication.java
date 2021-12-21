d51c1f54788a912a41c874089deccb2a5f4c6955
Josh Moscoe <jmoscoe@manifestcorp.com>
2021-12-21 09:42:54 -0500

    New project

---
+++ src/main/java/com/manifestcorp/com/onsoracledb/OnsOracleDbApplication.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.com.onsoracledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsOracleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsOracleDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifestcorp/com/onsoracledb/OnsOracleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.com.onsoracledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsOracleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
